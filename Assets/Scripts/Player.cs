dde7362ae6cf4a19b52fa75ad64ea0dcfec2573d
kumatch <taku12261929@gmail.com>
2020-01-05 13:45:20 +0900

    その1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D Rigidbody;
+
+    [SerializeField]
+    private float MaxSpeed = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(Rigidbody.velocity.x) > MaxSpeed)
+        {
+            Rigidbody.velocity =
+                new Vector2(
+                    MaxSpeed * Mathf.Sign(Rigidbody.velocity.x),
+                    Rigidbody.velocity.y);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D Player;
+
+    [SerializeField]
+    private float Speed;
+
+    [SerializeField]
+    private float JumpPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Player.AddForce(Vector2.left * Speed);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            Player.AddForce(Vector2.right * Speed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Player.AddForce(Vector2.up * JumpPower);
+        }
+    }
+}
